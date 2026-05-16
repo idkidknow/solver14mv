38d4b2fffec15da3b1e7db8eae4f622d7bb10071
idkana <ifidkidknow@gmail.com>
2026-05-16 16:40:50 +0800

    organize components

--- solver14mv/src/solver14mv/ui/App.scala
+++ solver14mv/src/solver14mv/ui/App.scala
@@ -45,81 +45,94 @@ object App {
       cellSafety.set(Array.fill(m, n)(CellSafety.Indeterminate))
     }
 
+    val header = Header()
+
+    val boardSettingsInput = BoardSettingsInput(
+      BoardSettingsInput.settings --> Observer.combine(
+        m.writer.contramap[Settings](_.row),
+        n.writer.contramap[Settings](_.col),
+        mineCount.writer.contramap[Settings](_.mineCount),
+      ),
+      Signal.combine(m, n).changes.distinct --> { case (m, n) => reset(m, n) },
+    )
+
+    val ruleEditor = RuleEditor(RuleEditor.rules --> rules.writer)
+
     val clueBrush = Var(Clue.None)
     val addNumber = EventBus[Int]()
-    val cluesInput = div(
-      ClueBrushEditor(
-        ClueBrushEditor.clueBrush --> clueBrush,
-        addNumber.stream --> ClueBrushEditor.addNumber,
-      ),
-      MinesweeperGrid(clues.signal, cellSafety.signal)(
-        MinesweeperGrid.onClick --> clues.updater[(Int, Int)] {
-          case (grid, (i, j)) =>
-            grid.updated(
-              i,
-              grid(i).updated(j, clueBrush.now()),
-            )
+    val clueBrushEditor = ClueBrushEditor(
+      ClueBrushEditor.clueBrush --> clueBrush,
+      addNumber.stream --> ClueBrushEditor.addNumber,
+    )
+    val brushEditShortcut = modSeq(
+      onKeyDown.filter(_.code === "Minus").mapTo(-1) --> addNumber,
+      onKeyDown.filter(_.code === "Equal").mapTo(1) --> addNumber,
+    )
+
+    val minesweeperGrid = MinesweeperGrid(clues.signal, cellSafety.signal)(
+      MinesweeperGrid.onClick --> clues.updater[(Int, Int)] {
+        case (grid, (i, j)) =>
+          grid.updated(
+            i,
+            grid(i).updated(j, clueBrush.now()),
+          )
+      }
+    )
+
+    val resetButton = Button(variant = "secondary")(
+      onClick --> { _ => reset(m.now(), n.now()) },
+      "reset",
+    )
+
+    val solveButton = Button()(
+      "solve",
+      onClick --> { _ =>
+        val solving: IO[Unit] = solver
+          .solve[IO](clues.now(), rules.now(), mineCount.now())
+          .foreach { result =>
+            IO.delay {
+              cellSafety.update { prev =>
+                prev.updated(
+                  result.i,
+                  prev(result.i).updated(result.j, result.safety),
+                )
+              }
+            }
+          }
+          .compile
+          .drain
+        val finish = IO.delay {
+          runningSolverCancel.set(None)
         }
-      ),
+        val io = for {
+          _ <- IO
+            .delay(runningSolverCancel.now().getOrElse(().pure[IO]))
+            .flatten
+          fiber <- (solving *> finish).start
+          _ <- IO.delay(runningSolverCancel.set(Some(fiber.cancel)))
+        } yield ()
+        dispatcher.unsafeRunAndForget(io)
+      },
+      disabled <-- miniZincInitialized.signal.not,
+    )
+
+    val stopButton = Button(variant = "secondary")(
+      "stop",
+      onClick --> { _ => stopSolver() },
+      disabled <-- runningSolverCancel.signal.map(_.isEmpty),
     )
 
     div(
       initMiniZinc,
-      onKeyDown.filter(_.code === "Minus").mapTo(-1) --> addNumber,
-      onKeyDown.filter(_.code === "Equal").mapTo(1) --> addNumber,
-      Header(),
-      BoardSettingsInput(
-        BoardSettingsInput.settings --> Observer.combine(
-          m.writer.contramap[Settings](_.row),
-          n.writer.contramap[Settings](_.col),
-          mineCount.writer.contramap[Settings](_.mineCount),
-        )
-      ),
-      Signal.combine(m, n).changes.distinct --> { case (m, n) => reset(m, n) },
-      RuleEditor(
-        RuleEditor.rules --> rules.writer
-      ),
-      cluesInput,
-      Button(variant = "secondary")(
-        onClick --> { _ => reset(m.now(), n.now()) },
-        "reset",
-      ),
-      Button()(
-        "solve",
-        onClick --> { _ =>
-          val solving: IO[Unit] = solver
-            .solve[IO](clues.now(), rules.now(), mineCount.now())
-            .foreach { result =>
-              IO.delay {
-                cellSafety.update { prev =>
-                  prev.updated(
-                    result.i,
-                    prev(result.i).updated(result.j, result.safety),
-                  )
-                }
-              }
-            }
-            .compile
-            .drain
-          val finish = IO.delay {
-            runningSolverCancel.set(None)
-          }
-          val io = for {
-            _ <- IO
-              .delay(runningSolverCancel.now().getOrElse(().pure[IO]))
-              .flatten
-            fiber <- (solving *> finish).start
-            _ <- IO.delay(runningSolverCancel.set(Some(fiber.cancel)))
-          } yield ()
-          dispatcher.unsafeRunAndForget(io)
-        },
-        disabled <-- miniZincInitialized.signal.not,
-      ),
-      Button(variant = "secondary")(
-        "stop",
-        onClick --> { _ => stopSolver() },
-        disabled <-- runningSolverCancel.signal.map(_.isEmpty),
-      ),
+      header,
+      boardSettingsInput,
+      ruleEditor,
+      clueBrushEditor,
+      brushEditShortcut,
+      minesweeperGrid,
+      resetButton,
+      solveButton,
+      stopButton,
     )
   }
 }
